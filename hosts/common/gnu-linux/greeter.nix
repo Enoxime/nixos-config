9c2b3679c4c4e2ecc702f6c7872866c673f6bd5d
Enoxime <4358598+Enoxime@users.noreply.github.com>
2026-07-23 20:58:36 -0400

    chore: flake upgrade

--- hosts/common/gnu-linux/greeter.nix
+++ hosts/common/gnu-linux/greeter.nix
@@ -1,25 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   environment.systemPackages = [
     pkgs.tuigreet
   ];
-
-  # programs.regreet = {
-  #   enable = true;
-  #   # font = {
-  #   #   name = "";
-  #   # };
-  # };
-
-  # # Hyprland config for greetd.regreet
-  # hyprlanRegreet = pkgs.writeText "hyprland_regreet"
-  # ''
-  #   exec-once = regreet; hyprctl dispatch exit
-  #   misc {
-  #     disable_hyprland_logo = true
-  #     disable_splash_rendering = true
-  #     disable_hyprland_qtutils_check = true
-  #   }
-  # '';
 
   # Enable Display Manager
   services.greetd = {
@@ -27,13 +9,13 @@
     settings = {
       default_session = {
         # https://github.com/apognu/tuigreet?tab=readme-ov-file
-        command = ''
-          ${pkgs.tuigreet}/bin/tuigreet \
-            --time --time-format '%H:%M | %a • %h | %F' \
-            --cmd Hyprland
-        '';
-        # command = "Hyprland --config ${hyprlanRegreet}";
-        # user = "${username}";
+        command = lib.concatStrings [
+          "${pkgs.tuigreet}/bin/tuigreet "
+          "--time --time-format '%H:%M | %a • %h | %F' "
+          "--power-shutdown 'systemctl poweroff' "
+          "--power-reboot 'systemctl reboot' "
+          "--cmd start-hyprland"
+        ];
         user = "greeter";
       };
     };
